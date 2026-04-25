91703c252c0609dc7eadfd4b80fef2e398856ae0
Ryuz <ryuzer@proton.me>
2026-04-25 17:23:38 +0200

    fix(build): ignore ilha:pages

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "@fontsource-variable/geist";
 import "./app.css";
 import "@wterm/dom/css";
 import "basecoat-css/all";
+// @ts-ignore
 import { pageRouter } from "ilha:pages";
 
 pageRouter.mount("#app");
